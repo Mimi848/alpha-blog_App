6e20abb774a16983363f33800797d937a3cbd30a
Musaddiq Arif <Musaddiqmimi1000@gmail.com>
2020-12-28 15:39:43 +0500

    added update and edit routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,40 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def index
+    @article = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+      @article = Article.find(params[:id])
+      if @article.update(params.require(:article).permit(:title, :description))
+        flash[:notce] = "Article was successfully updated"
+        redirect_to @article
+      else
+        flash[:error] = "Something went wrong"
+        render 'edit'
+      end
+  end
+  
+  
+
+  def create
+      @article = Article.new(params.require(:article).permit(:title, :description))
+      if @article.save
+        flash[:notice] = "Article was created successfully."
+        redirect_to @article
+      else
+        render 'new'
+      end
+  end
+  
+  
 end
