7262f22871081a01c0cc8bf8df370eb93f647cfa
Musaddiq Arif <Musaddiqmimi1000@gmail.com>
2020-12-30 16:50:04 +0500

    add users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
 
     def new
-      @user = User.new
+        @user = User.new
     end
+    def show
+        @user = User.find(params[:id])
+        @articles = @user.articles
+    end
+    
 
 
   def edit
